03350e8c9bb7d339ff7fa00d5dd09bd1f1456572
Hita Barasm Mamagani <barasm.hita@outlook.com>
2021-10-08 21:50:48 +0330

    Initialized project

---
+++ agents/navigation/behavior_types.py
@@ -0,0 +1,49 @@
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+""" This module contains the different parameters sets for each behavior. """
+
+
+class Cautious(object):
+    """Class for Cautious agent."""
+    max_speed = 40
+    speed_lim_dist = 6
+    speed_decrease = 12
+    safety_time = 3
+    min_proximity_threshold = 12
+    braking_distance = 6
+    tailgate_counter = 0
+
+
+class Normal(object):
+    """Class for Normal agent."""
+    max_speed = 50
+    speed_lim_dist = 3
+    speed_decrease = 10
+    safety_time = 3
+    min_proximity_threshold = 10
+    braking_distance = 5
+    tailgate_counter = 0
+
+
+class Aggressive(object):
+    """Class for Aggressive agent."""
+    max_speed = 70
+    speed_lim_dist = 1
+    speed_decrease = 8
+    safety_time = 3
+    min_proximity_threshold = 8
+    braking_distance = 4
+    tailgate_counter = -1
+
+
+class Custom(object):
+    """Class for Custom agent."""
+    def __init__(self, speed):
+        self.max_speed = speed
+        self.speed_lim_dist = 1
+        self.speed_decrease = 10
+        self.safety_time = 3
+        self.min_proximity_threshold = 10
+        self.braking_distance = 7
+        self.tailgate_counter = 0
